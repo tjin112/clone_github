dff812235e182ceb569745bb230951ee74b51225
”tjin112” <940986798@qq.com>
2021-05-17 03:06:04 -0700

    set up koa-session and redis

--- server.js
+++ server.js
@@ -1,34 +1,69 @@
-const Koa = require('koa')
-const next = require('next')
-const Router = require('koa-router')
-
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const Koa = require("koa");
+const next = require("next");
+const Router = require("koa-router");
+const session = require("koa-session");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const RedisSessionStore = require('./server/session-store')
+const Redis = require('ioredis')
 // 处理http请求的响应
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
 
-app.prepare().then(()=>{
-    const server = new Koa()
-    const router = new Router()
+//创建redis client
+const redis = new Redis()
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+  server.keys = ["taimin develpo github app"];
+  const SESSION_CONFIG = {
+    key: "jid",
+    store:new RedisSessionStore(redis)
+  };
+  server.use(session(SESSION_CONFIG, server));
+  server.use(async (ctx, next) => {
+    // console.log(ctx.cookies.get('id'))
+    // if(!ctx.session.user){
+    //     ctx.session.user = {
+    //         name:'taimin',
+    //         age:18
+    //     }
+    // }
+    // else{
+        console.log(ctx.session.user)
+    // }
 
-    router.get('/a/:id',async (ctx)=>{
-        const id = ctx.params.id
-        await handle(ctx.req,ctx.res,{
-            pathname:'/a',
-            query:{
-                id
-            }
-        })
-        ctx.respond = false
-    })
+    await next();
+  });
+  router.get("/a/:id", async (ctx) => {
+    const id = ctx.params.id;
+    await handle(ctx.req, ctx.res, {
+      pathname: "/a",
+      query: {
+        id,
+      },
+    });
+    ctx.respond = false;
+  });
+  router.get("/set/user", async (ctx) => {
+    ctx.session.user = {
+      name: "taimin",
+      age: 18,
+    };
+    ctx.body = 'set session successfully'
+  });
+  router.get("/del/user", async (ctx) => {
+    ctx.session = null
+    ctx.body = 'set session successfully'
+  });
 
-    server.use(router.routes())
+  server.use(router.routes());
 
-    server.use(async (ctx,next)=>{
-        await handle(ctx.req,ctx.res)
-        ctx.req = false
-    })
-    server.listen(3000,()=>{
-        console.log('Koa server listening on 3000')
-    })
-})
+  server.use(async (ctx, next) => {
+    // ctx.cookies.set("id", "userid:123");
+    await handle(ctx.req, ctx.res);
+    ctx.req = false;
+  });
+  server.listen(3000, () => {
+    console.log("Koa server listening on 3000");
+  });
+});
